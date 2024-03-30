bc5aa9e6adf12915f9950c8d6cd6cd8986b77cb3
dimitrmo <dmouratidis153@gmail.com>
2024-03-30 12:41:08 +0200

    Include a simple test

---
+++ src/utils/utils.spec.ts
@@ -0,0 +1,23 @@
+import { isWebsocketConnectionOptions } from "./utils";
+
+describe('isWebsocketConnectionOptions', () => {
+
+    const connection1 = {
+        channels: [],
+        endpoints: [],
+    };
+
+    const connection2 = {
+        channels: [],
+        endpoint: "",
+    };
+
+    it('identifies correctly a websocket connection', () => {
+        expect(isWebsocketConnectionOptions(connection1)).toBe(true);
+    });
+
+    it('does not identify correctly a websocket connection', () => {
+        expect(isWebsocketConnectionOptions(connection2)).toBe(false);
+    });
+
+});
