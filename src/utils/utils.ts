2fab0a81d8e5c4955f038808e2b214f6051ae8b9
dimitrmo <dmouratidis153@gmail.com>
2024-03-30 12:25:28 +0200

    Upgrade to latest

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,10 +1,6 @@
 import type { WebsocketConnectionOptions } from "@rhiaqey/sdk-ts";
 
-export function format(first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
-}
-
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const isWebsocketConnectionOptions = (obj: any): obj is WebsocketConnectionOptions => {
-  return obj.endpoint !== undefined && obj.channels !== undefined;
+  return obj.endpoints !== undefined && obj.channels !== undefined;
 };
